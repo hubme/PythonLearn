9b966969fba377a8552749267a16660a6ba234c0
VanceKing <huogxu@gmail.com>
2020-05-14 19:04:07 +0800

    yield 生成器

--- sample/sample_yield.py
+++ sample/sample_yield.py
@@ -1,18 +1,37 @@
+# https://blog.csdn.net/mieleizhi0522/article/details/82142856
+
 def foo():
     print("starting...")
     while True:
         res = yield 4
-        print("res:",res)
+        print("res:", res)
+
+
 g = foo()
 
 # @1 首次 next foo()，打印"starting..."，运行到 " res = yield 4"，返回 4，挂起
-print(next(g)) 
+print(next(g))
 # @2 打印 20 个星号
 print("*"*20)
 # @3 又 next foo()，从挂起的位置接着运行，打印“res: None”。又运行到 "res = yield 4"，返回 4，挂起
 print(next(g))
 # 关闭生成器
 g.close()
+print("\n")
+
+
+def foo2(num):
+    print("starting...")
+    while num < 10:
+        num += 1
+        yield num
+
+
+f = foo2(0)
+for n in f:
+    print(n)
+f.close()
+
 
 '''
 def echo(value=None):
