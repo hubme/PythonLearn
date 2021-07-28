10d7b29677f4452c8665512359643f4585febae8
VanceKing <huogxu@gmail.com>
2021-07-28 17:11:53 +0800

    yield

--- sample/sample_yield.py
+++ sample/sample_yield.py
@@ -1,4 +1,9 @@
-# https://blog.csdn.net/mieleizhi0522/article/details/82142856
+"""
+https://blog.csdn.net/mieleizhi0522/article/details/82142856
+
+yield from 是在Python3.3才出现的语法。
+yield from 后面需要加的是可迭代对象，它可以是普通的可迭代对象，也可以是迭代器，甚至是生成器。
+"""
 
 def foo():
     print("starting...")
@@ -7,31 +12,65 @@ def foo():
         print("res:", res)
 
 
-g = foo()
-
-# @1 首次 next foo()，打印"starting..."，运行到 " res = yield 4"，返回 4，挂起
-print(next(g))
-# @2 打印 20 个星号
-print("*"*20)
-# @3 又 next foo()，从挂起的位置接着运行，打印“res: None”。又运行到 "res = yield 4"，返回 4，挂起
-print(next(g))
-# 关闭生成器
-g.close()
-print("\n")
-
-
 def foo2(num):
     print("starting...")
     while num < 10:
         num += 1
         yield num
 
+def test1():
+    g = foo()
+    # <generator object foo at 0x0000000003172A50>
+    print(g)
 
-f = foo2(0)
-for n in f:
-    print(n)
-f.close()
+    # @1 首次 next foo()，打印"starting..."，运行到 " res = yield 4"，返回 4，挂起
+    print(next(g))
+    # @2 打印 20 个星号
+    print("*"*20)
+    # @3 又 next foo()，从挂起的位置接着运行，打印“res: None”。又运行到 "res = yield 4"，返回 4，挂起
+    print(next(g))
+    # 关闭生成器
+    g.close()
+    print("\n")
 
+def test2():
+    f = foo2(0)
+    for n in f:
+        print(n)
+    f.close()
+
+def gen_yield(*args, **kw):
+    for item in args:
+        for i in item:
+            yield i
+
+def gen_yield_from(*args, **kw):
+    for item in args:
+        # yield from 后面加上可迭代对象
+        yield from item
+
+def test3():
+    astr='ABC'
+    alist=[1,2,3]
+    adict={"name":"wangbm","age":18}
+    agen=(i for i in range(4,8))
+
+    new_list1=gen_yield(astr, alist, adict, agen)
+    # <generator object gen at 0x0000000003162B30>
+    print(new_list1)
+    print(list(new_list1))
+    
+    print()
+    
+    new_list2=gen_yield(astr, alist, adict, agen)
+    # <generator object gen at 0x0000000003162B30>
+    print(new_list2)
+    print(list(new_list2))
+    
+    
+
+if __name__ == '__main__':
+    test3()
 
 '''
 def echo(value=None):
