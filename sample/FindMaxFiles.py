d2834be84428a4a12cd0238363d6382129673ac5
VanceKing <huogxu@gmail.com>
2022-05-21 15:41:10 +0800

    os.walk()

--- sample/FindMaxFiles.py
+++ sample/FindMaxFiles.py
@@ -1,5 +1,4 @@
 import os
-import time
 
 from os.path import join, getsize
 from heapq import nlargest
@@ -23,8 +22,20 @@ def largest_files(n: int, start_at: str):
         print(f'{size/MB} MB {path}')
 
 
+def test_os_walk(dir_path: str):
+    """
+    https://docs.python.org/zh-cn/3/library/os.html#os.walk
+    os.walk(file_path) 返回生成器类型，三元组; dirpath: 根目录路径; dirnames: 目录下所有的文件夹; filenames: 目录下所有的文件夹
+    """
+    dir = os.walk(dir_path)
+    print(type(dir))
+    for dirpath, dirnames, filenames in dir:
+        print(f'root: {dirpath}, dirnames: {dirnames}, files: {filenames}')
+
+
 if __name__ == '__main__':
-    start = time.perf_counter()
+    """ start = time.perf_counter()
     largest_files(10, "C:/Windows")
     elapsed = time.perf_counter() - start
-    print(f'{elapsed} seconds elapsed')
+    print(f'{elapsed} seconds elapsed') """
+    test_os_walk('/home/vance/Android/AndroidProjects/PythonLearn/')
